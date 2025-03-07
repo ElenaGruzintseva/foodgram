47bbd1b9ba3ac2703d2b6ce159f4b022d20330a7
elenagruzintseva <elenagruzinceva@yandex.ru>
2025-03-07 12:58:37 +0300

    recipe

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Recipe(models.Model):
+
+    tags = models.ManyToManyField(Tag, verbose_name='Тэг')
+    ingredients = models.ManyToManyField(
+        Ingredient, through='RecipeIngredient'
+    )
+    text = models.TextField('Описание')
+    name = models.CharField('Название рецепта', max_length=MAX_CHAR_LENGTH)
+    image = models.ImageField('Фотография', upload_to='recipes/images/')
+    cooking_time = models.IntegerField('Время приготовления')
+    author = models.ForeignKey(
+        User,
+        related_name='recipes',
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+    )
+
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
