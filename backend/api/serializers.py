f4322f30e3ae46796f36ca03c26e0c52b41db49a
elenagruzintseva <elenagruzinceva@yandex.ru>
2025-03-08 14:44:55 +0300

    serializers recipe

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,14 +1,34 @@
+
+from rest_framework import serializers
+
+from users.serializers import Base64ImageField
+from recipes.models import (
+    Ingredient,
+    Recipe,
+    Tag
+)
+
+
 class RecipeReadSerializer(serializers.ModelSerializer):
 
+    image = Base64ImageField(read_only=True)
+    name = serializers.ReadOnlyField()
+    cooking_time = serializers.ReadOnlyField()
 
-class UserGETSerializer(djoser.serializers.UserSerializer):
+    class Meta:
+        fields = ('id', 'name', 'image', 'cooking_time')
+        model = Recipe
 
 
-class SubscriptionSerializer(serializers.ModelSerializer):
-    is_subscribed = serializers.SerializerMethodField(read_only=True)
-    recipes = serializers.SerializerMethodField(read_only=True)
-    email = serializers.ReadOnlyField()
-    username = serializers.ReadOnlyField()
-    first_name = serializers.ReadOnlyField()
-    last_name = serializers.ReadOnlyField()
-    avatar = serializers.ImageField(read_only=True)
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Tag
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Ingredient
---
+++ backend/users/serializers.py
@@ -0,0 +1,150 @@
+import base64
+
+from django.core.files.base import ContentFile
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
+from recipes.models import (
+    Recipe
+)
+from users.models import Subscribe, User
+from api.serializers import RecipeReadSerializer
+
+
+class Base64ImageField(serializers.ImageField):
+
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+
+        return super().to_internal_value(data)
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField(read_only=True)
+    recipes = serializers.SerializerMethodField(read_only=True)
+    email = serializers.ReadOnlyField()
+    username = serializers.ReadOnlyField()
+    first_name = serializers.ReadOnlyField()
+    last_name = serializers.ReadOnlyField()
+    avatar = serializers.ImageField(read_only=True)
+    recipes_count = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        fields = (
+            'id',
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed',
+            'recipes',
+            'recipes_count',
+            'avatar',
+        )
+        model = User
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+
+        if request and not request.user.is_anonymous:
+            user = request.user
+            return user.followers.filter(author=obj).exists()
+
+        return False
+
+    def get_recipes_count(self, obj):
+        return Recipe.objects.filter(author=obj).count()
+
+    def get_recipes(self, obj):
+        request = self.context.get('request')
+        recipes_limit = request.query_params.get('recipes_limit', None)
+
+        if recipes_limit is not None:
+            recipes = Recipe.objects.filter(author=obj)[: int(recipes_limit)]
+        else:
+            recipes = Recipe.objects.filter(author=obj)
+
+        serializer = RecipeReadSerializer(
+            recipes, many=True, context=self.context
+        )
+        return serializer.data
+
+
+class SubscriptionCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscribe
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Subscribe.objects.all(),
+                fields=('user', 'author'),
+                message='Вы уже подписаны',
+            )
+        ]
+
+    def validate(self, obj):
+        user = self.context.get('request').user
+        author = obj['author']
+
+        if user == author:
+            raise serializers.ValidationError(
+                'Нельзя подписываться на самого себя'
+            )
+
+        return obj
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        return SubscriptionSerializer(
+            instance.author, context={'request': request}
+        ).data
+
+
+class AvatarUserSerializer(serializers.ModelSerializer):
+    avatar = Base64ImageField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('avatar',)
+
+
+class UserGETSerializer(djoser.serializers.UserSerializer):
+
+    is_subscribed = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        fields = (
+            'id',
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed',
+            'avatar',
+        )
+        model = User
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+
+        if request and not request.user.is_anonymous:
+            user = request.user
+            return user.followers.filter(author=obj).exists()
+        return False
+
+
+class UserCreateSerializer(djoser.serializers.UserCreateSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'password',
+            'avatar',
+        )
+        model = User
