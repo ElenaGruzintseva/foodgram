391d4e709047bfec288b884a5f50137fc87b2fa9
elenagruzintseva <elenagruzinceva@yandex.ru>
2025-03-10 15:47:59 +0300

    urls api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,42 @@
 from django.urls import include, path
-
 from rest_framework import routers
+
+from .views import (
+    IngredientViewSet,
+    RecipeViewSet,
+    TagViewSet,
+)
+from users.views import (
+    MeView,
+    SubscribeView,
+    SubscriptionsListView,
+    UserAvatar
+    )
 
 router = routers.DefaultRouter()
 
 router.register(r'tags', TagViewSet, basename='tags')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
+router.register(r'ingredients', IngredientViewSet, basename='ingredients')
+
+
+user_urls = [
+    path('me/', MeView.as_view(), name='user-me'),
+    path(
+        'subscriptions/',
+        SubscriptionsListView.as_view(),
+        name='subscriptions-list',
+    ),
+    path('<int:pk>/subscribe/', SubscribeView.as_view(), name='subscribe'),
+    path(
+        'me/avatar/',
+        UserAvatar.as_view({'put': 'update', 'delete': 'destroy'}),
+        name='user-avatar'),
+]
+
+urlpatterns = [
+    path('users/', include(user_urls)),
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+]
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Subscribe, User
+
+admin.site.empty_value_display = 'Не задано'
+
+
+@admin.register(User)
+class MyUserAdmin(admin.ModelAdmin):
+
+    list_display = ('username', 'first_name', 'last_name', 'email', 'avatar',)
+    list_filter = ('username', 'email')
+    search_fields = ('username',)
+
+
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+
+    list_display = ('user', 'author')
+    list_filter = ('user', 'author')
+    search_fields = ('user', 'author')
