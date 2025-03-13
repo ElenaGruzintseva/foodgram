6bac45939b2727f39be493097288e070b2e1631c
elenagruzintseva <elenagruzinceva@yandex.ru>
2025-03-13 17:43:19 +0300

    run commands

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,8 +11,8 @@ done
 python manage.py makemigrations users --no-input
 python manage.py makemigrations recipes --no-input
 python manage.py migrate --no-input
-# python manage.py loaddata data/ingredients.csv
-# python manage.py createsuperuser
+python manage.py load_data data/ingredients.csv
+python manage.py create_superuser -u
 python manage.py collectstatic --no-input
 
 exec gunicorn -w 2 -b 0.0.0.0:7000 foodgram.wsgi:application --access-logfile - --error-logfile -
