923f46847410a9c99403eabe70ee5f6027578f2b
elenagruzintseva <elenagruzinceva@yandex.ru>
2025-03-17 18:21:56 +0300

    media

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -100,10 +100,6 @@ AUTH_USER_MODEL = 'users.User'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'recipes', 'static'),
-]
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
