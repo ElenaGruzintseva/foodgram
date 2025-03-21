20920dd565d6ab4d596f9d2ae7f4f46f3db7d61d
elenagruzintseva <elenagruzinceva@yandex.ru>
2025-03-21 15:27:31 +0300

    recingr

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -189,15 +189,15 @@ class RecipeIngredient(Model):
         ordering = ('recipe',)
         verbose_name = 'Ингредиент для рецепта'
         verbose_name_plural = 'Ингредиенты для рецепта'
-        # constraints = (
-        #     UniqueConstraint(
-        #         fields=(
-        #             'recipe',
-        #             'ingredient',
-        #         ),
-        #         name='\n%(app_label)s_%(class)s ingredient alredy added\n',
-        #     ),
-        # )
+        constraints = (
+            UniqueConstraint(
+                fields=(
+                    'recipe',
+                    'ingredient',
+                ),
+                name='\n%(app_label)s_%(class)s ingredient alredy added\n',
+            ),
+        )
 
     def __str__(self):
         return f'{self.amount} {self.ingredient}'
@@ -224,7 +224,8 @@ class FavoriteRecipe(Model):
         verbose_name_plural = 'Избранные'
         constraints = [
             UniqueConstraint(
-                fields=['user', 'recipe'], name='unique_user_favorite_recipe'
+                fields=['user', 'recipe'],
+                name='unique_user_favorite_recipe',
             )
         ]
 
@@ -253,7 +254,8 @@ class ShoppingList(Model):
         verbose_name_plural = 'Списки покупок'
         constraints = [
             UniqueConstraint(
-                fields=['user', 'recipe'], name='unique_user_shopping_list'
+                fields=['user', 'recipe'],
+                name='unique_user_shopping_list',
             )
         ]
 
