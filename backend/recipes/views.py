3f6ba45eccd8da1087fae77ec23958d40b551a9b
elenagruzintseva <elenagruzinceva@yandex.ru>
2025-03-25 17:52:50 +0300

    short link

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,11 +1,11 @@
-from django.core.exceptions import ValidationError
 from django.shortcuts import redirect
 
 from .models import Recipe
 
 
 def redirect_to_recipe_detail(request, pk):
-    if Recipe.objects.filter(pk=pk).exists():
+    try:
+        recipe = Recipe.objects.get(pk=pk)
         return redirect(f'/recipes/{pk}/')
-    else:
-        raise ValidationError(f'Рецепт с id {pk} отсутствует.')
+    except Recipe.DoesNotExist:
+        return redirect('/not-found/')
