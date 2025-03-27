fe8dbef2c33a5e257180d421a92ddeee62f4cb34
elenagruzintseva <elenagruzinceva@yandex.ru>
2025-03-27 16:37:40 +0300

    commands ok

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -1,4 +1,5 @@
 import csv
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from pathlib import Path
@@ -7,21 +8,17 @@ from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-
     help = 'Загрузка данных из CSV-файлов'
 
     def bulk_create_ingredients(self, model, rows):
-        ingredients = []
-        for row in rows:
-            name, measurement_unit = row[0], row[1]
-            existing_ingredient = model.objects.filter(name=name).exists()
-            if not existing_ingredient:
-                ingredients.append(
-                    model(name=name, measurement_unit=measurement_unit)
-                )
+        ingredients = [
+            model(name=name, measurement_unit=measurement_unit)
+            for name, measurement_unit in rows
+        ]
 
-        model.objects.bulk_create(ingredients)
-        return len(ingredients)
+        model.objects.bulk_create(ingredients, ignore_conflicts=True)
+
+        return len(rows)
 
     def handle(self, *args, **options):
         csv_path = Path(settings.DATA_DIR) / 'ingredients.csv'
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as UserAdmins
+
 from .models import Subscribe, User
 
 admin.site.empty_value_display = 'Не задано'
