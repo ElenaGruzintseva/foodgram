b105437d50838c16c9ac424fe28a052827583194
elenagruzintseva <elenagruzinceva@yandex.ru>
2025-03-29 09:47:15 +0300

    6 test

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -12,5 +12,5 @@ class OwnerOnlyPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
+            or obj == request.user
         )
